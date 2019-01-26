8075e7342923482fa9c652f7262e283b2ee1cb96
Mark Zhang <z@cciex.com>
2019-01-26 10:29:42 +0800

    Commit at 2019-01-26 10:29:22

--- tmp/test.py
+++ tmp/test.py
@@ -1,17 +1,30 @@
-#! --*-- coding:utf-8 --*--
-import os
-import time
-import argparse
+#! coding:utf-8
+
+
+import asyncio
+from yeelib import search_bulbs
+
+from yeelib import search_bulbs
+
+
+@asyncio.coroutine
+def turn_all_lights_on(bulbs):
+    while True:
+        for b in bulbs.values():
+            asyncio.Task(b.send_command("set_power",
+                                        ["off", "sudden", 40]))
+        yield from asyncio.sleep(10)
+
 
 def main():
-    current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-a', '--all', help='Push all notes to github', action='store_true')
-    parser.add_argument('-d', '--dir', help='The target Dir you want to push', default='.')
-    parser.add_argument('comment', help='The git commit comments', nargs='?', default=f'{current_time}')
-    args = parser.parse_args()
+    loop = asyncio.get_event_loop()
+    bulbs = loop.run_until_complete(search_bulbs())
+    loop.create_task(turn_all_lights_on(bulbs))
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        loop.stop()
 
-    print(f" -a is {args.all} \n  -d is {args.dir} \n  -comment is {args.comment}")
 
 if __name__ == '__main__':
     main()
